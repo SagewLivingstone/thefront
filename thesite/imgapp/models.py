4da8d8d46bc74ddd3b0983dbeb1358e82df140fd
Sage <SageWLivingstone@gmail.com>
2023-04-19 21:04:49 -0700

    image model

--- thesite/imgapp/models.py
+++ thesite/imgapp/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Image(models.Model):
+    image = models.ImageField()
