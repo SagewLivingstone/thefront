eb7981a7a0f2971a8b87b08460d689bf0c081d2a
Sage <SageWLivingstone@gmail.com>
2023-04-21 17:42:42 -0700

    aws setup, with keys hidden

--- thesite/imgapp/models.py
+++ thesite/imgapp/models.py
@@ -5,4 +5,8 @@ from django.utils import timezone
 
 
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to="image")
+    caption = models.CharField(max_length=512)
+
+    def __str__(self) -> str:
+        return self.caption
--- thesite/thesite/settings.py
+++ thesite/thesite/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import configparser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Setup configuration from secrets
+config = configparser.ConfigParser()
+secrets_path = BASE_DIR / 'secrets.ini'
+config.read(secrets_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -23,20 +29,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rfezn8ev05qu^ppbtsq2jm3841bhmtghdyr((xo@i8ghu8l7t@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.getboolean('django', 'debug')
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
 INSTALLED_APPS = [
+    'imgapp.apps.ImgappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config.get('django', 'timezone')
 
 USE_I18N = True
 
@@ -116,6 +123,31 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+USE_S3 = config.getboolean('aws', 'use_s3')
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = config.get('aws', 'access_key_id')
+    AWS_SECRET_ACCESS_KEY = config.get('aws', 'secret_access_key')
+    AWS_STORAGE_BUCKET_NAME = config.get('aws', 'storage_bucket')
+    AWS_S3_REGION = config.get('aws', 'region')
+    AWS_S3_HOST = f's3.{AWS_S3_REGION}.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_S3_HOST}/{AWS_STORAGE_BUCKET_NAME}'
+    AWS_STATIC_LOCATION = 'static'
+    AWS_MEDIA_LOCATION = 'media'
+
+    # django file settings
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
