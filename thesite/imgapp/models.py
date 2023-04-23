a1eba64acbffad68f374479f044c1e8b9bdc28d8
Sage <SageWLivingstone@gmail.com>
2023-04-22 23:35:40 -0700

    initial image resize code

--- thesite/imgapp/models.py
+++ thesite/imgapp/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
 from django.db import models
-from django.utils import timezone
+from django.core.files.base import ContentFile
+import PIL.Image
+from io import BytesIO
+import os
 
 from .imgmeta import GetExif
 
@@ -10,14 +13,22 @@ class Image(models.Model):
     """
     Contains a single image
     """
-    image = models.ImageField(upload_to="image")
-    caption = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # Full-size image
+    image =             models.ImageField(upload_to="image")
+    # 1440p resized image
+    image_normal =      models.ImageField(upload_to="image_normal", null=True, editable=False)
+    # small resized image
+    image_thumbnail =   models.ImageField(upload_to="image_thumbnail", null=True, editable=False)
+    caption =           models.TextField()
+    created_at =        models.DateTimeField(auto_now_add=True)
+    updated_at =        models.DateTimeField(auto_now=True)
 
     exifdata = None
 
     def load_metadata_dict(self):
+        """
+        Load metadata before image has been sent to storage
+        """
         if (not hasattr(self.image.file, 'image')): return
         self.exifdata = GetExif(self.image.file.image)
 
@@ -30,11 +41,45 @@ class Image(models.Model):
             self.metadata = ImageMetadata(image=self)
         self.metadata.fill(self.exifdata)
         self.metadata.save()
+    
+    def make_resizes(self):
+        image = PIL.Image.open(self.image)
+        image.thumbnail((1920, 1080), PIL.Image.ANTIALIAS)
+        
+        thumb_name, thumb_extension = os.path.splitext(self.image.name)
+        thumb_extension = thumb_extension.lower()
+
+        thumb_filename = thumb_name + '_normal' + thumb_extension
+
+        if thumb_extension in ['.jpeg', '.jpg']:
+            FTYPE = 'JPEG'
+        elif thumb_extension in ['.gif']:
+            FTYPE = 'GIF'
+        elif thumb_extension in ['.png']:
+            FTYPE = 'PNG'
+        else:
+            return False  # Unrecognized file type
+        
+        # Save thumbnail to memory
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+        
+        # set save=False to avoid an infinite loop
+        self.image_normal.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+
+        return True
+
 
     def save(self, *args, **kwargs):
-        self.load_metadata_dict()
+        # Check if we have a new file to process
+        if not self.image.closed:
+            self.load_metadata_dict()
+            self.make_resizes()
         super(Image, self).save(*args, **kwargs)
-        self.update_metadata()
+        if not self.image.closed:
+            self.update_metadata()
 
     def __str__(self) -> str:
         return self.caption
@@ -43,13 +88,13 @@ class ImageMetadata(models.Model):
     """
     Metadata for a single image
     """
-    capture_date = models.DateTimeField()
-    camera = models.CharField(max_length=80)
-    lens = models.CharField(max_length=80)
-    focallength = models.CharField(max_length=10)
-    fnumber = models.CharField(max_length=10)
-    shutterspeed = models.CharField(max_length=10)
-    iso = models.CharField(max_length=10)
+    capture_date =  models.DateTimeField()
+    camera =        models.CharField(max_length=80)
+    lens =          models.CharField(max_length=80)
+    focallength =   models.CharField(max_length=10)
+    fnumber =       models.CharField(max_length=10)
+    shutterspeed =  models.CharField(max_length=10)
+    iso =           models.CharField(max_length=10)
     
     image = models.OneToOneField(
         Image,
